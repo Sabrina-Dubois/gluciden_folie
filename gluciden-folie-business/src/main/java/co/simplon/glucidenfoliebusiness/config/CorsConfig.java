6d57a4ddf150653018425aba72e67f8a50a72cce
Dubois Sabrina <dubois.sabrina84@gmail.com>
2025-07-29 20:36:10 +0200

    desactivation des cors en prod

--- gluciden-folie-business/src/main/java/co/simplon/glucidenfoliebusiness/config/CorsConfig.java
+++ gluciden-folie-business/src/main/java/co/simplon/glucidenfoliebusiness/config/CorsConfig.java
@@ -3,10 +3,12 @@ package co.simplon.glucidenfoliebusiness.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("local")
 public class CorsConfig {
 
 	@Value("${gluciden-folie-business.cors.allowedOrigins}")
